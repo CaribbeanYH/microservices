5944e4970b05cee78f53a9135472076471d2e89d
CaribbeanHY <321175302@qq.com>
2024-08-15 01:23:13 +0800

    fix

--- easy-microservice-hystrix/src/main/java/com/easy/architecture/easymicroservicehystrix/EasyMicroserviceHystrixApplication.java
+++ easy-microservice-hystrix/src/main/java/com/easy/architecture/easymicroservicehystrix/EasyMicroserviceHystrixApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @Description
  * @date 2024/7/28 19:09
  */
+@EnableHystrixDashboard
 @EnableHystrix
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.easy.architecture"})
