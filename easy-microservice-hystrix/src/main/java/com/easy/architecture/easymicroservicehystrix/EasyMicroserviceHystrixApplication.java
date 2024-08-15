9d9fd124bd426d28e465149a07e2e198917850ba
yanghai10 <yanghai10@jd.com>
2024-08-15 16:47:52 +0800

    fix

--- easy-microservice-hystrix/src/main/java/com/easy/architecture/easymicroservicehystrix/EasyMicroserviceHystrixApplication.java
+++ easy-microservice-hystrix/src/main/java/com/easy/architecture/easymicroservicehystrix/EasyMicroserviceHystrixApplication.java
@@ -1,11 +1,14 @@
 package com.easy.architecture.easymicroservicehystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -24,6 +27,21 @@ public class EasyMicroserviceHystrixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyMicroserviceHystrixApplication.class, args);
+    }
+
+    /**
+     * 获取 HystrixMetricsStreamServlet 的 ServletRegistrationBean 对象
+     *
+     * @return ServletRegistrationBean<HystrixMetricsStreamServlet> 返回 HystrixMetricsStreamServlet 的 ServletRegistrationBean 对象
+     */
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
