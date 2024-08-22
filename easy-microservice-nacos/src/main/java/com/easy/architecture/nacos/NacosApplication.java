dc080787b58f8e65c3ed173002c6a822a3cfdfd3
CaribbeanHY <321175302@qq.com>
2024-08-23 01:19:26 +0800

    fix

---
+++ easy-microservice-nacos/src/main/java/com/easy/architecture/nacos/NacosApplication.java
@@ -0,0 +1,20 @@
+package com.easy.architecture.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yanghai
+ * @ClassName
+ * @Description
+ * @date 2024/8/23 01:16
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+}
