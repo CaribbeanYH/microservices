1e9645b55f8357394fb8c8fb28f123cbb5605352
CaribbeanHY <321175302@qq.com>
2024-08-23 23:40:43 +0800

    fix

---
+++ easy-architecture-netty/src/main/java/com/easy/architecture/io/tcp/NettyTcpServer.java
@@ -0,0 +1,57 @@
+package com.easy.architecture.io.tcp;
+
+import com.easy.architecture.io.tcp.handler.NettyTcpServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author yanghai
+ * @ClassName
+ * @Description
+ * @date 2024/8/23 23:20
+ */
+public class NettyTcpServer {
+
+    private final int port;
+
+    public NettyTcpServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyTcpServerHandler()); // channel to handler
+                        }
+                    });
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        NettyTcpServer server = new NettyTcpServer(port);
+        server.start();
+    }
+}
