596f5e3de068151617803187d67cad1a2b7e1a0e
yanghai10 <yanghai10@jd.com>
2024-11-16 19:06:58 +0800

    fix

--- easy-architecture-netty/src/main/java/com/easy/architecture/io/netty/inoutbound/PipelineServer.java
+++ easy-architecture-netty/src/main/java/com/easy/architecture/io/netty/inoutbound/PipelineServer.java
@@ -11,6 +11,9 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  * @ClassName
  * @Description
  * @date 2024/9/21 01:45
+ * <p>
+ *     结论：ctx.channel().writeAndFlush 将从 Pipeline 的尾部开始往前找 OutboundHandler 。 ctx.writeAndFlush 会从当前 handler 往前找 OutboundHandler。
+ * </p>
  */
 public class PipelineServer {
     public void start(int port) throws Exception {
@@ -43,13 +46,14 @@ public class PipelineServer {
 
 
 //                            // 注册两个InboundHandler，执行顺序为注册顺序，所以应该是InboundHandler1 InboundHandler2
-//                            ch.pipeline().addLast(new InboundHandler1());
-//                            ch.pipeline().addLast(new InboundHandler2());
-//                            // 注册两个OutboundHandler，执行顺序为注册顺序的逆序，所以应该是OutboundHandler2 OutboundHandler1
-//                            ch.pipeline().addLast(new OutboundHandler1());
-//                            ch.pipeline().addLast(new OutboundHandler2());
+                            ch.pipeline().addLast(new InboundHandler1());
+                            ch.pipeline().addLast(new InboundHandler2());
+                            // 注册两个OutboundHandler，执行顺序为注册顺序的逆序，所以应该是OutboundHandler2 OutboundHandler1
+                            ch.pipeline().addLast(new OutboundHandler1());
+                            ch.pipeline().addLast(new OutboundHandler2());
 //                            //InboundHandler1 --> InboundHandler2 --> OutboundHandler1 --> OutboundHandler2
 //                            //InboundHandler2 是ctx.writeAndFlush(msg); 会从InboundHandler2开始找out，没有了out了
+//                            //InboundHandler2 是ctx.channel().writeAndFlush(msg); 会从OutboundHandler2开始找out，没有了out了
                         }
                     }).option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
